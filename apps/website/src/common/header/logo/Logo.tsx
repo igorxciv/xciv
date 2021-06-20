d12b164014a45981d9a25dea4b165772d1a9c5a0
igorxciv <icheliadinski@gmail.com>
2021-06-20 14:57:38 -0400

    chg: add animation for mobile menu

--- apps/website/src/common/header/logo/Logo.tsx
+++ apps/website/src/common/header/logo/Logo.tsx
@@ -1,12 +1,18 @@
 import classNames from 'classnames'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Logo.module.scss'
 
 export const Logo: FC = () => {
+  const [appeared, setIsAppeared] = useState(false)
+
   const logoClasses = classNames(styles['logo'])
-  const title1Classes = classNames(styles['logo__title1'])
-  const title2Classes = classNames(styles['logo__title2'])
+  const title1Classes = classNames(styles['logo__title1'], { [styles['logo__title1--appeared']]: appeared })
+  const title2Classes = classNames(styles['logo__title2'], { [styles['logo__title2--appeared']]: appeared })
+
+  useEffect(() => {
+    setIsAppeared(true)
+  }, [])
 
   return (
     <Link to="/" className={logoClasses}>
---
+++ libs/ui-kit/src/lib/__design/trumps/README.md
@@ -0,0 +1,1 @@
+### utils, helpers, classes visibility, alt colors
