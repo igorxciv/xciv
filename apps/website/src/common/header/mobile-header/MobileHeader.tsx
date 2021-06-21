47c6e64216681b40b7438436cab580c7303de1be
igorxciv <icheliadinski@gmail.com>
2021-06-21 19:31:26 -0400

    chg: get rid of animation deps

--- apps/website/src/common/header/mobile-header/MobileHeader.tsx
+++ apps/website/src/common/header/mobile-header/MobileHeader.tsx
@@ -1,7 +1,5 @@
 import { createRef, FC, useCallback, useState } from 'react'
-import { CSSTransition } from 'react-transition-group'
 import classNames from 'classnames'
-import { transitionClasses } from '@ui-kit/utils'
 import { MobileMenu } from './mobile-menu'
 import { Hamburger } from './hamburger'
 import styles from './MobileHeader.module.scss'
@@ -14,16 +12,15 @@ export const MobileHeader: FC = () => {
     setIsMobileMenuOpen((isOpen) => !isOpen)
   }, [])
 
-  const mobileMenuTransitionClasses = transitionClasses(styles, 'mobile-header__mobile-menu')
   const mobileMenuClasses = classNames(styles['mobile-header__mobile-menu'])
   const hamburgerClasses = classNames(styles['mobile-header__hamburger'])
+
+  const mobileMenu = isMobileMenuOpen ? <MobileMenu ref={mobileMenuRef} className={mobileMenuClasses} /> : null
 
   return (
     <>
       <Hamburger crossed={isMobileMenuOpen} onClick={handleHamburgerClick} className={hamburgerClasses} />
-      <CSSTransition in={isMobileMenuOpen} timeout={200} classNames={mobileMenuTransitionClasses} nodeRef={mobileMenuRef} unmountOnExit>
-        <MobileMenu ref={mobileMenuRef} className={mobileMenuClasses} />
-      </CSSTransition>
+      {mobileMenu}
     </>
   )
 }
--- libs/ui-kit/src/lib/utils/index.ts
+++ libs/ui-kit/src/lib/utils/index.ts
@@ -1,4 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const noop = (): void => {}
-
-export { transitionClasses } from './transition-classes'
