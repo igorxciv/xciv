072eb6b6f1dbe06aa3cb0378a14bb0524924cd79
igorxciv <icheliadinski@gmail.com>
2021-06-30 15:17:01 -0400

    new: add link

--- apps/website/src/common/header/logo/Logo.tsx
+++ apps/website/src/common/header/logo/Logo.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from '@website/components'
 import styles from './Logo.module.scss'
 
 export const Logo: FC = () => {
---
+++ apps/website/src/components/link/Link.tsx
@@ -0,0 +1,22 @@
+import { FC, useCallback } from 'react'
+import { Link as RouterLink, LinkProps, useLocation } from 'react-router-dom'
+
+export const Link: FC<LinkProps> = (props: LinkProps) => {
+  const { children, to, onClick, ...restProps } = props
+  const { pathname } = useLocation()
+
+  const handleClick = useCallback((event) => {
+    if (pathname === to) {
+      event.preventDefault()
+    }
+    if (onClick) {
+      onClick(event)
+    }
+  }, [onClick, to, pathname])
+
+  return (
+    <RouterLink to={to} onClick={handleClick} {...restProps}>
+      {children}
+    </RouterLink>
+  )
+}
