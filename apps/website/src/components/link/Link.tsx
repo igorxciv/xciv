f580d629e28d2f78e1de9781c7f29c94e1ea8673
igorxciv <icheliadinski@gmail.com>
2021-06-30 15:22:34 -0400

    new: add typing

--- apps/website/src/components/link/Link.tsx
+++ apps/website/src/components/link/Link.tsx
@@ -1,11 +1,11 @@
-import { FC, useCallback } from 'react'
+import { FC, useCallback, MouseEvent } from 'react'
 import { Link as RouterLink, LinkProps, useLocation } from 'react-router-dom'
 
 export const Link: FC<LinkProps> = (props: LinkProps) => {
   const { children, to, onClick, ...restProps } = props
   const { pathname } = useLocation()
 
-  const handleClick = useCallback((event) => {
+  const handleClick = useCallback((event: MouseEvent<HTMLAnchorElement>) => {
     if (pathname === to) {
       event.preventDefault()
     }
